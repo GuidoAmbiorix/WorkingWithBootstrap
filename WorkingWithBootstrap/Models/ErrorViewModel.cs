a31d16432e525e44ac513f5ad6dd3d4937756efe
GuidoAmbiorix <84160468+GuidoAmbiorix@users.noreply.github.com>
2022-02-14 20:53:29 -0400

    Add project files.

---
+++ WorkingWithBootstrap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorkingWithBootstrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
